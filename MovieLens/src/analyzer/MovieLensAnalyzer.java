8aebf30ab778f156b0a56f8c49191c8e032e22f4
Jared Polonitza <jaredpolonitza@Jareds-MacBook-Pro.local>
2018-11-13 22:38:21 -0800

    Working graph creation methods

--- MovieLens/src/analyzer/MovieLensAnalyzer.java
+++ MovieLens/src/analyzer/MovieLensAnalyzer.java
@@ -1,15 +1,135 @@
 package analyzer;
 
+import data.Movie;
+import data.Reviewer;
+import graph.Graph;
+import util.DataLoader;
+import util.PriorityQueue;
+import java.util.ArrayList;
+
+import java.util.Map;
+
 public class MovieLensAnalyzer {
 	
 	public static void main(String[] args){
+
 		// Your program should take two command-line arguments: 
 		// 1. A ratings file
-		// 2. A movies file with information on each movie e.g. the title and genres		
+		// 2. A movies file with information on each movie e.g. the title and genres
+		
 		if(args.length != 2){
 			System.err.println("Usage: java MovieLensAnalyzer [ratings_file] [movie_title_file]");
 			System.exit(-1);
-		}		
-		// FILL IN THE REST OF YOUR PROGRAM
+		}
+		int option = 3;
+		String movieLocS = "movies.csv/";
+		String ratingsLocS = "ratings.csv/";
+		String dataDirS = "/MovieLens/src/ml-latest-small/";
+		String dir = System.getProperty("user.dir");
+
+		DataLoader data = new DataLoader();
+		data.loadData(dir + dataDirS + movieLocS,  dir + dataDirS + ratingsLocS);
+		Graph<Movie> graph = new Graph<>();
+		Map<Integer,Movie> movies = data.getMovies();
+		Map<Integer,Reviewer> reviewers = data.getReviewers();
+		//Fill graph
+		for (int i = 0; i < movies.size() + 1; i++) {
+			graph.addVertex(movies.get(i));
+		}
+		//Set edges
+		PriorityQueue priorityQueue = new PriorityQueue();
+		ArrayList<Reviewer> userWeight = new ArrayList<>();
+		for (Integer i : reviewers.keySet()) {
+			Reviewer r = reviewers.get(i);
+			priorityQueue.push(r.numRated(),r.getReviewerId());
+		}
+		for (int i = 0; i < priorityQueue.size(); i++) {
+			int p = priorityQueue.topElement();
+			userWeight.add(reviewers.get(p));
+			priorityQueue.pop();
+		}
+		//Option 1 ~ 10 seconds
+		if (option == 1) {
+			for (Integer a : movies.keySet()) {
+				for (Integer b : movies.keySet()) {
+					Movie u = movies.get(a);
+					Movie v = movies.get(b);
+					if (v.getMovieId() != u.getMovieId()) {
+						int common = 0;
+						int ind = 0;
+						int set = 12;
+						while (common < set && ind < userWeight.size() - 1) {
+							Reviewer r = userWeight.get(ind);
+							if (r.ratedMovie(u.getMovieId()) && r.ratedMovie(v.getMovieId())) {
+								if (r.getMovieRating(u.getMovieId()) == r.getMovieRating(v.getMovieId())) {
+									common++;
+								}
+							}
+							ind++;
+						}
+						if (common == set) {
+							graph.addEdge(u, v);
+						}
+					}
+				}
+			}
+		}
+		else if (option == 2) {
+			//Option 2 ~ 10 seconds
+			for (Integer a : movies.keySet()) {
+				for (Integer b : movies.keySet()) {
+					Movie u = movies.get(a);
+					Movie v = movies.get(b);
+					if (v.getMovieId() != u.getMovieId()) {
+						int common = 0;
+						int ind = 0;
+						int set = 12;
+						while (common < set && ind < userWeight.size() - 1) {
+							Reviewer r = userWeight.get(ind);
+							if (r.ratedMovie(u.getMovieId()) && r.ratedMovie(v.getMovieId())) {
+								common++;
+							}
+							ind++;
+						}
+						if (common == set) {
+							graph.addEdge(u, v);
+						}
+					}
+				}
+			}
+		}
+		else {
+			//Option 3 ~ 30 seconds
+			for (Integer a : movies.keySet()) {
+				for (Integer b : movies.keySet()) {
+					Movie u = movies.get(a);
+					Movie v = movies.get(b);
+					if (v.getMovieId() != u.getMovieId()) {
+						int common = 0;
+						int ind = 0;
+						int pool = 0;
+						while (ind < userWeight.size() - 1) {
+							Reviewer r = userWeight.get(ind);
+							if (r.ratedMovie(u.getMovieId()) && r.ratedMovie(v.getMovieId())) {
+								pool++;
+								if (r.getMovieRating(u.getMovieId()) == r.getMovieRating(v.getMovieId())) {
+									common++;
+								}
+							}
+							ind++;
+						}
+						if (pool > 0) {
+							if (common/pool >= 0.33) {
+								graph.addEdge(u, v);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		System.out.println(graph);
+		System.out.println(graph.numVertices());
+		System.out.println(graph.numEdges());
 	}
 }

55ed588693d1c354a320347a29e1dcc62577e886
Lukas Jimenez-Smith <lukasjimenez-smith@lukass-mbp-15.lan>
2018-11-14 17:50:07 -0800

    To Pull

--- MovieLens/src/graph/GraphAlgorithms.java
+++ MovieLens/src/graph/GraphAlgorithms.java
@@ -1,9 +1,24 @@
 package graph;
 
+import java.util.PriorityQueue;
 
 public class GraphAlgorithms {
 	public dijkstrasAlgorithm(Graph<Integer> graph, int source){
-        
+		PriorityQueue Q = new PriorityQueue<Movie>();
+
+		int[] dist = new int[graph.numEdges()];
+
+		for (int i = 0; i < dist.length; i++){
+			dist[i] = Integer.MAX_VALUE;
+		}
+
+		Movie prev = new Movie();
+		prev = null;
+
+		dist[source] = 0;
+
+		
+		
 	}
 	
 }
